7b037d5a755ca1cd3a8c33c20a783f46c928c740
abdallahmehanna11136-collab <abdallahmehanna11136@gmail.com>
2026-05-30 01:31:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -107,3 +107,6 @@ def download_video(video_id):
 if __name__ == '__main__':
     app.run()
     # test
+@app.route('/googleb1247b73-2e6f-486c-a765-4a22f28a5254.html')
+def google_verify():
+    return "google-site-verification: googleb1247b73-2e6f-486c-a765-4a22f28a5254.html"
